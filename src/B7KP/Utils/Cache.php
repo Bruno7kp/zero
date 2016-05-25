367aef14831211dbc28bdbd9761b762a4c4d3732
Bruno Camargo <bruno7kp@outlook.com>
2016-05-25 01:14:55 -0300

    Att

--- src/B7KP/Utils/Cache.php
+++ src/B7KP/Utils/Cache.php
@@ -8,26 +8,27 @@ class Cache
 
 	static function getJsonData($class, $file = "main")
 	{
-		$class = str_replace("\\", "-", $class);
-		$filejson = MAIN_DIR."/cache/".$class."/".$file.".json";
-		$content = file_get_contents($filejson);
-		return json_decode($content);
+		$filejson = MAIN_DIR."cache/".$class."/".$file.".json";
+		if(file_exists($filejson))
+		{
+			$content = file_get_contents($filejson);
+			return json_decode($content);
+		}
 	}
 
 	static function setJsonData($class, $data, $file = "main")
 	{
-		$dir = MAIN_DIR."/cache/".$class;
+		$dir = MAIN_DIR."cache/".$class;
 		if(!file_exists($dir))
 		{
 			mkdir($dir, 0777, true);
 		}
 		$filejson = $dir."/".$file.".json";
-		if(file_exists($filejson))
-		{
-			$fp = fopen($filejson, 'w');
-			fwrite($fp, json_encode($data));
-			fclose($fp);
-		}
+		
+		$fp = fopen($filejson, 'w');
+		fwrite($fp, json_encode($data));
+		fclose($fp);
+		
 	}
 }
 ?>
