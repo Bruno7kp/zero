275971b5567802f460182fc0ac31094921883aa8
Bruno Camargo <bruno7kp@outlook.com>
2016-06-04 11:02:32 -0300

    0.07.650

--- src/B7KP/Core/App.php
+++ src/B7KP/Core/App.php
@@ -41,7 +41,7 @@ class App
 	{
 		self::$name = "ZERO";
 		self::$author = "Bruno7kp";
-		self::$version = "0.07.481";
+		self::$version = "0.07.650";
 
 		switch ($_SERVER['SERVER_NAME']) {
 			case 'localhost':
