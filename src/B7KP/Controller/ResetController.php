bd81f3aed9990c6262fbdfacb3286591592c9022
Bruno Camargo <bruno7kp@outlook.com>
2018-09-15 10:27:11 -0300

    cache on browser

--- src/B7KP/Controller/ResetController.php
+++ src/B7KP/Controller/ResetController.php
@@ -2,102 +2,89 @@
 namespace B7KP\Controller;
 
 use B7KP\Core\Dao;
+use B7KP\Entity\User;
+use B7KP\Library\Lang;
+use B7KP\Library\Route;
 use B7KP\Model\Model;
 use B7KP\Utils\UserSession;
-use B7KP\Utils\Login;
-use B7KP\Library\Assert;
-use B7KP\Library\Route;
-use B7KP\Library\Lang;
-use B7KP\Entity\User;
-
-use League\Flysystem\Adapter\Local;
-use League\Flysystem\Filesystem;
-use Cache\Adapter\Filesystem\FilesystemCachePool;
 
 class ResetController extends Controller
 {
-	private $user, $settings;
-	private $pool;
+    private $user, $settings;
 
-	function __construct(Model $factory)
-	{
-		parent::__construct($factory);
-		$this->user = UserSession::getUser($this->factory);
-		$filesystemAdapter = new Local(MAIN_DIR.'cache');
-		$filesystem        = new Filesystem($filesystemAdapter);
-		$this->pool = new FilesystemCachePool($filesystem);
-	}
+    public function __construct(Model $factory)
+    {
+        parent::__construct($factory);
+        $this->user = UserSession::getUser($this->factory);
+    }
 
-	/**
-	* @Route(name=reset_acc|route=/edit/reset)
-	*/
-	public function indexAction()
-	{
-		$this->checkAccess();
-		$this->render("reset.php");
-	}
+    /**
+     * @Route(name=reset_acc|route=/edit/reset)
+     */
+    public function indexAction()
+    {
+        $this->checkAccess();
+        $this->render("reset.php");
+    }
 
-	protected function checkAccess()
-	{
-		if(!$this->user instanceof User)
-		{
-			$this->redirectToRoute("login");
-		}
-	}
+    protected function checkAccess()
+    {
+        if (!$this->user instanceof User) {
+            $this->redirectToRoute("login");
+        }
+    }
 
-	/**
-	* @Route(name=reset_profile|route=/check/reset)
-	*/
-	public function reset()
-	{
-		$this->removeWeeklyCharts();
-		echo json_encode(array("error" => 0, "msg" => Lang::get("acc_success")));
-	}
+    /**
+     * @Route(name=reset_profile|route=/check/reset)
+     */
+    public function reset()
+    {
+        $this->removeWeeklyCharts();
+        echo json_encode(array("error" => 0, "msg" => Lang::get("acc_success")));
+    }
 
-	/**
-	* @Route(name=delete_profile|route=/check/delete)
-	*/
-	public function delete()
-	{
-		$this->removeWeeklyCharts();
-		$this->factory->removeBy("\B7KP\Entity\Settings", "iduser", $this->user->id);
-		$this->factory->removeBy("\B7KP\Entity\Plaque", "iduser", $this->user->id);
-		$this->factory->removeBy("\B7KP\Entity\User", "id", $this->user->id);
-		
-		echo json_encode(array("error" => 0, "msg" => Lang::get("acc_success_rem")));
-	}
+    /**
+     * @Route(name=delete_profile|route=/check/delete)
+     */
+    public function delete()
+    {
+        $this->removeWeeklyCharts();
+        $this->factory->removeBy("\B7KP\Entity\Settings", "iduser", $this->user->id);
+        $this->factory->removeBy("\B7KP\Entity\Plaque", "iduser", $this->user->id);
+        $this->factory->removeBy("\B7KP\Entity\User", "id", $this->user->id);
 
-	/**
-	* @Route(name=delete_plaques|route=/check/delete_plaques)
-	*/
-	public function deletePlaques()
-	{
-		$this->factory->removeBy("\B7KP\Entity\Plaque", "iduser", $this->user->id);
-		
-		echo json_encode(array("error" => 0, "msg" => Lang::get("acc_success_pla")));
-	}
+        echo json_encode(array("error" => 0, "msg" => Lang::get("acc_success_rem")));
+    }
 
-	private function removeWeeklyCharts()
-	{
-		$dao = Dao::getConn();
+    /**
+     * @Route(name=delete_plaques|route=/check/delete_plaques)
+     */
+    public function deletePlaques()
+    {
+        $this->factory->removeBy("\B7KP\Entity\Plaque", "iduser", $this->user->id);
 
-		$affected = $dao->run("SELECT t.* FROM artist_charts t, week w, user u WHERE t.idweek = w.id AND u.id = w.iduser AND w.iduser = ".$this->user->id);
-		foreach ($affected as $key => $value) {
-			$this->factory->removeBy("\B7KP\Entity\Artist_charts", "id", $value->id);
-		}
+        echo json_encode(array("error" => 0, "msg" => Lang::get("acc_success_pla")));
+    }
 
-		$affected = $dao->run("SELECT t.* FROM album_charts t, week w, user u WHERE t.idweek = w.id AND u.id = w.iduser AND w.iduser = ".$this->user->id);
-		foreach ($affected as $key => $value) {
-			$this->factory->removeBy("\B7KP\Entity\Album_charts", "id", $value->id);
-		}
+    private function removeWeeklyCharts()
+    {
+        $dao = Dao::getConn();
 
-		$affected = $dao->run("SELECT t.* FROM music_charts t, week w, user u WHERE t.idweek = w.id AND u.id = w.iduser AND w.iduser = ".$this->user->id);
-		foreach ($affected as $key => $value) {
-			$this->factory->removeBy("\B7KP\Entity\Music_charts", "id", $value->id);
-		}
+        $affected = $dao->run("SELECT t.* FROM artist_charts t, week w, user u WHERE t.idweek = w.id AND u.id = w.iduser AND w.iduser = " . $this->user->id);
+        foreach ($affected as $key => $value) {
+            $this->factory->removeBy("\B7KP\Entity\Artist_charts", "id", $value->id);
+        }
 
-		$this->factory->removeBy("\B7KP\Entity\Week", "iduser", $this->user->id);
-		$login = md5($this->user->login);
-		$this->pool->invalidateTags([$login."_album", $login."_music", $login."_artist"]);
-	}
+        $affected = $dao->run("SELECT t.* FROM album_charts t, week w, user u WHERE t.idweek = w.id AND u.id = w.iduser AND w.iduser = " . $this->user->id);
+        foreach ($affected as $key => $value) {
+            $this->factory->removeBy("\B7KP\Entity\Album_charts", "id", $value->id);
+        }
+
+        $affected = $dao->run("SELECT t.* FROM music_charts t, week w, user u WHERE t.idweek = w.id AND u.id = w.iduser AND w.iduser = " . $this->user->id);
+        foreach ($affected as $key => $value) {
+            $this->factory->removeBy("\B7KP\Entity\Music_charts", "id", $value->id);
+        }
+
+        $this->factory->removeBy("\B7KP\Entity\Week", "iduser", $this->user->id);
+    }
 }
--- src/B7KP/Entity/Settings.php
+++ src/B7KP/Entity/Settings.php
@@ -1,165 +1,165 @@
 <?php
 namespace B7KP\Entity;
 
-use B7KP\Utils\Constants as C;
 use B7KP\Library\Lang;
+use B7KP\Utils\Constants as C;
 
 class Settings extends Entity
 {
-	/**
-	* @Assert(null=false|int)
-	*/
-	protected $iduser;
+    /**
+     * @Assert(null=false|int)
+     */
+    protected $iduser;
 
-	/**
-	* @Assert(null=false|int|option)
-	* @Options(values={"10": "Top 10","5": "Top 5", "15": "Top 15", "20": "Top 20", "25": "Top 25", "30": "Top 30", "40": "Top 40", "50": "Top 50"})
-	*/
-	protected $art_limit;
+    /**
+     * @Assert(null=false|int|option)
+     * @Options(values={"10": "Top 10","5": "Top 5", "15": "Top 15", "20": "Top 20", "25": "Top 25", "30": "Top 30", "40": "Top 40", "50": "Top 50"})
+     */
+    protected $art_limit;
 
-	/**
-	* @Assert(null=false|int|option)
-	* @Options(values={"10": "Top 10","5": "Top 5", "15": "Top 15", "20": "Top 20", "25": "Top 25", "30": "Top 30", "40": "Top 40", "50": "Top 50"})
-	*/
-	protected $alb_limit;
+    /**
+     * @Assert(null=false|int|option)
+     * @Options(values={"10": "Top 10","5": "Top 5", "15": "Top 15", "20": "Top 20", "25": "Top 25", "30": "Top 30", "40": "Top 40", "50": "Top 50"})
+     */
+    protected $alb_limit;
 
-	/**
-	* @Assert(null=false|int|option)
-	* @Options(values={"10": "Top 10","5": "Top 5", "15": "Top 15", "20": "Top 20", "25": "Top 25", "30": "Top 30", "40": "Top 40", "50": "Top 50"})
-	*/
-	protected $mus_limit;
+    /**
+     * @Assert(null=false|int|option)
+     * @Options(values={"10": "Top 10","5": "Top 5", "15": "Top 15", "20": "Top 20", "25": "Top 25", "30": "Top 30", "40": "Top 40", "50": "Top 50"})
+     */
+    protected $mus_limit;
 
-	/**
-	* @Assert(null=false|int|option)
-	* @Options(values={"0": "No","1": "Yes"}|settings={"translate": "1"})
-	*/
-	protected $show_images;
+    /**
+     * @Assert(null=false|int|option)
+     * @Options(values={"0": "No","1": "Yes"}|settings={"translate": "1"})
+     */
+    protected $show_images;
 
-	/**
-	* @Assert(null=false|int|option)
-	* @Options(values={"0": "No","1": "Yes"}|settings={"translate": "1"})
-	*/
-	protected $show_dropouts;
+    /**
+     * @Assert(null=false|int|option)
+     * @Options(values={"0": "No","1": "Yes"}|settings={"translate": "1"})
+     */
+    protected $show_dropouts;
 
-	/**
-	* @Assert(null=false|int|option)
-	* @Options(values={"1": "Yes","0": "No"}|settings={"translate": "1"})
-	*/
-	protected $show_first_image;
+    /**
+     * @Assert(null=false|int|option)
+     * @Options(values={"1": "Yes","0": "No"}|settings={"translate": "1"})
+     */
+    protected $show_first_image;
 
-	/**
-	* @Assert(null=false|int|option)
-	* @Options(values={"1": "sett_diff_lw","0": "sett_none","2": "sett_lw", "3": "sett_pp"}|settings={"translate": "1"})
-	*/
-	protected $show_move;
+    /**
+     * @Assert(null=false|int|option)
+     * @Options(values={"1": "sett_diff_lw","0": "sett_none","2": "sett_lw", "3": "sett_pp"}|settings={"translate": "1"})
+     */
+    protected $show_move;
 
-	/**
-	* @Assert(null=false|int|option)
-	* @Options(values={"1": "Yes","0": "No"}|settings={"translate": "1"})
-	*/
-	protected $show_playcounts;
+    /**
+     * @Assert(null=false|int|option)
+     * @Options(values={"1": "Yes","0": "No"}|settings={"translate": "1"})
+     */
+    protected $show_playcounts;
 
-	/**
-	* @Assert(null=false|int|option)
-	* @Options(values={"0": "Português","1": "English"})
-	*/
-	protected $lang;
+    /**
+     * @Assert(null=false|int|option)
+     * @Options(values={"0": "Português","1": "English"})
+     */
+    protected $lang;
 
-	/**
-	* @Assert(null=false|minNum=0|int)
-	*/
-	protected $alb_cert_gold;
+    /**
+     * @Assert(null=false|minNum=0|int)
+     */
+    protected $alb_cert_gold;
 
-	/**
-	* @Assert(null=false|int|biggerThan=alb_cert_gold)
-	*/
-	protected $alb_cert_platinum;
+    /**
+     * @Assert(null=false|int|biggerThan=alb_cert_gold)
+     */
+    protected $alb_cert_platinum;
 
-	/**
-	* @Assert(null=false|int|biggerThan=alb_cert_platinum)
-	*/
-	protected $alb_cert_diamond;
+    /**
+     * @Assert(null=false|int|biggerThan=alb_cert_platinum)
+     */
+    protected $alb_cert_diamond;
 
-	/**
-	* @Assert(null=false|minNum=0|int)
-	*/
-	protected $mus_cert_gold;
+    /**
+     * @Assert(null=false|minNum=0|int)
+     */
+    protected $mus_cert_gold;
 
-	/**
-	* @Assert(null=false|int|biggerThan=mus_cert_gold)
-	*/
-	protected $mus_cert_platinum;
+    /**
+     * @Assert(null=false|int|biggerThan=mus_cert_gold)
+     */
+    protected $mus_cert_platinum;
 
-	/**
-	* @Assert(null=false|int|biggerThan=mus_cert_platinum)
-	*/
-	protected $mus_cert_diamond;
+    /**
+     * @Assert(null=false|int|biggerThan=mus_cert_platinum)
+     */
+    protected $mus_cert_diamond;
 
-	/**
-	* @Assert(null=false|int|option)
-	* @Options(values={"1": "Yes","0": "No"}|settings={"translate": "1"})
-	*/
-	protected $show_cert;
+    /**
+     * @Assert(null=false|int|option)
+     * @Options(values={"1": "Yes","0": "No"}|settings={"translate": "1"})
+     */
+    protected $show_cert;
 
-	/**
-	* @Assert(null=false|int|option)
-	* @Options(values={"1": "Yes","0": "No"}|settings={"translate": "1"})
-	*/
-	protected $show_chart_cert;
+    /**
+     * @Assert(null=false|int|option)
+     * @Options(values={"1": "Yes","0": "No"}|settings={"translate": "1"})
+     */
+    protected $show_chart_cert;
 
-	/**
-	* @Assert(null=false|int|option)
-	* @Options(values={"1": "Yes","0": "No"}|settings={"translate": "1"})
-	*/
-	protected $show_plaque;
+    /**
+     * @Assert(null=false|int|option)
+     * @Options(values={"1": "Yes","0": "No"}|settings={"translate": "1"})
+     */
+    protected $show_plaque;
 
-	/**
-	* @Assert(null=false|int|option)
-	* @Options(values={"1": "pt_x","0": "play_x","2": "both_x"}|settings={"translate": "1"})
-	*/
-	protected $cert_type;
+    /**
+     * @Assert(null=false|int|option)
+     * @Options(values={"1": "pt_x","0": "play_x","2": "both_x"}|settings={"translate": "1"})
+     */
+    protected $cert_type;
 
-	/**
-	* @Assert(null=false|int|option)
-	* @Options(values={"1": "Yes","0": "No"}|settings={"translate": "1"})
-	*/
-	protected $show_times;
+    /**
+     * @Assert(null=false|int|option)
+     * @Options(values={"1": "Yes","0": "No"}|settings={"translate": "1"})
+     */
+    protected $show_times;
 
-	/**
-	* @Assert(null=false|int|option)
-	* @Options(values={"1": "Yes","0": "No"}|settings={"translate": "1"})
-	*/
-	protected $show_points;
+    /**
+     * @Assert(null=false|int|option)
+     * @Options(values={"1": "Yes","0": "No"}|settings={"translate": "1"})
+     */
+    protected $show_points;
 
-	/**
-	* @Assert(null=false|int|option)
-	* @Options(values={"1": "Yes","0": "No"}|settings={"translate": "1"})
-	*/
-	protected $hide_livechart;
+    /**
+     * @Assert(null=false|int|option)
+     * @Options(values={"1": "Yes","0": "No"}|settings={"translate": "1"})
+     */
+    protected $hide_livechart;
 
-	/**
-	* @Assert(null=false|int|option)
-	* @Options(values={"1": "Dark","0": "Light"}|settings={"translate": "1"})
-	*/
-	protected $theme;
+    /**
+     * @Assert(null=false|int|option)
+     * @Options(values={"1": "Dark","0": "Light"}|settings={"translate": "1"})
+     */
+    protected $theme;
 
-	/**
-	* @Assert(null=false|int|option)
-	* @Options(values={"0": "Sunday","1": "Friday"}|settings={"translate": "1"})
-	*/
-	protected $start_day;
+    /**
+     * @Assert(null=false|int|option)
+     * @Options(values={"0": "Sunday","1": "Friday"}|settings={"translate": "1"})
+     */
+    protected $start_day;
 
-	/**
-	* @Assert(null=false|int|option)
-	* @Options(values={"0": "Public","1": "Friends_only"}|settings={"translate": "1"})
-	*/
-	protected $visibility;
+    /**
+     * @Assert(null=false|int|option)
+     * @Options(values={"0": "Public","1": "Friends_only"}|settings={"translate": "1"})
+     */
+    protected $visibility;
 
-	/**
-	* @Assert(null=false|int|option)
-	* @Options(values={"1": "Yes","0": "No"}|settings={"translate": "1"})
-	*/
-	protected $show_wkl_cert;
+    /**
+     * @Assert(null=false|int|option)
+     * @Options(values={"1": "Yes","0": "No"}|settings={"translate": "1"})
+     */
+    protected $show_wkl_cert;
 
     /**
      * @Assert(null=true)
@@ -197,142 +197,138 @@ class Settings extends Entity
      */
     protected $weight_mus_pts;
 
-	function __construct()
-	{
-		parent::__construct();
-	}
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
-	public function __get($property)
-	{
-		if(!isset($this->$property) || is_null($this->$property))
-		{
-			return self::defaultValueFor($property);
-		}
-		else
-		{
-			return $this->$property;
-		}
-	}
+    public function __get($property)
+    {
+        if (!isset($this->$property) || is_null($this->$property)) {
+            return self::defaultValueFor($property);
+        } else {
+            return $this->$property;
+        }
+    }
 
-	static function getAllDefaults($username = "")
-	{
-		$def = new \stdClass();
+    public static function getAllDefaults($username = "")
+    {
+        $def = new \stdClass();
         $def->cert_name = $username;
-		$items = array(
-			"art_limit", 
-			"alb_limit", 
-			"mus_limit", 
-			"show_images", 
-			"show_dropouts", 
-			"show_first_image", 
-			"show_move", 
-			"show_playcounts", 
-			"lang", 
-			"alb_cert_gold", 
-			"alb_cert_platinum", 
-			"alb_cert_diamond", 
-			"mus_cert_gold", 
-			"mus_cert_platinum", 
-			"mus_cert_diamond", 
-			"show_cert", 
-			"show_chart_cert", 
-			"show_plaque", 
-			"cert_type", 
-			"show_times", 
-			"show_points", 
-			"hide_livechart", 
-			"theme", 
-			"start_day", 
-			"visibility", 
-			"show_wkl_cert", 
-			"custom_unity", 
-			"plaque_type",
-			"weight_alb_pls",
-			"weight_alb_pts",
-			"weight_mus_pls",
-			"weight_mus_pts"
-		);
+        $items = array(
+            "art_limit",
+            "alb_limit",
+            "mus_limit",
+            "show_images",
+            "show_dropouts",
+            "show_first_image",
+            "show_move",
+            "show_playcounts",
+            "lang",
+            "alb_cert_gold",
+            "alb_cert_platinum",
+            "alb_cert_diamond",
+            "mus_cert_gold",
+            "mus_cert_platinum",
+            "mus_cert_diamond",
+            "show_cert",
+            "show_chart_cert",
+            "show_plaque",
+            "cert_type",
+            "show_times",
+            "show_points",
+            "hide_livechart",
+            "theme",
+            "start_day",
+            "visibility",
+            "show_wkl_cert",
+            "custom_unity",
+            "plaque_type",
+            "weight_alb_pls",
+            "weight_alb_pts",
+            "weight_mus_pls",
+            "weight_mus_pts",
+        );
 
-		foreach ($items as $value) {
-			$def->$value = self::defaultValueFor($value);
-		}
+        foreach ($items as $value) {
+            $def->$value = self::defaultValueFor($value);
+        }
 
-		return $def;
-	}
+        return $def;
+    }
 
-	static function defaultValueFor($for)
-	{
-		switch ($for) {
-			case 'alb_cert_gold':
-			case 'alb_cert_platinum':
-			case 'alb_cert_diamond':
-			case 'mus_cert_gold':
-			case 'mus_cert_platinum':
-			case 'mus_cert_diamond':
-			case 'show_cert':
-			case 'show_chart_cert':
-			case 'show_plaque':
-			case 'cert_type':
-			case 'show_times':
-			case 'hide_livechart':
-			case 'theme':
-			case 'start_day':
-			case 'visibility':
-			case 'show_wkl_cert':
-			case 'plaque_type':
-				$for = 0;
-				break;
-			case 'show_points':
-			case 'weight_alb_pls':
-			case 'weight_alb_pts':
-			case 'weight_mus_pls':
-			case 'weight_mus_pts':
-				$for = 1;
-				break;
-			case 'art_limit':
-			case 'alb_limit':
-			case 'mus_limit':
-				$for = 10;
-				break;
+    public static function defaultValueFor($for)
+    {
+        switch ($for) {
+            case 'alb_cert_gold':
+            case 'alb_cert_platinum':
+            case 'alb_cert_diamond':
+            case 'mus_cert_gold':
+            case 'mus_cert_platinum':
+            case 'mus_cert_diamond':
+            case 'show_cert':
+            case 'show_chart_cert':
+            case 'show_plaque':
+            case 'cert_type':
+            case 'show_times':
+            case 'hide_livechart':
+            case 'theme':
+            case 'start_day':
+            case 'visibility':
+            case 'show_wkl_cert':
+            case 'plaque_type':
+                $for = 0;
+                break;
+            case 'show_points':
+            case 'weight_alb_pls':
+            case 'weight_alb_pts':
+            case 'weight_mus_pls':
+            case 'weight_mus_pts':
+                $for = 1;
+                break;
+            case 'art_limit':
+            case 'alb_limit':
+            case 'mus_limit':
+                $for = 10;
+                break;
 
-			case 'style':
-				$for = 'chart.css';
-				break;
+            case 'style':
+                $for = 'chart.css';
+                break;
 
-			case 'show_images':
-				$for = false;
-				break;
+            case 'show_images':
+                $for = false;
+                break;
 
-			case 'show_dropouts':
-				$for = false;
-				break;
+            case 'show_dropouts':
+                $for = false;
+                break;
 
-			case 'show_first_image':
-				$for = true;
-				break;
+            case 'show_first_image':
+                $for = true;
+                break;
 
-			case 'show_move':
-				$for = C::SHOW_MOVE_PERC;
-				break;
+            case 'show_move':
+                $for = C::SHOW_MOVE_PERC;
+                break;
 
-			case 'show_playcounts':
-				$for = true;
-				break;
+            case 'show_playcounts':
+                $for = true;
+                break;
 
-			case 'lang':
-				$for = Lang::detectLang();
-				break;
+            case 'lang':
+                $for = Lang::detectLang();
+                break;
 
             case 'custom_unity':
                 $for = "Plays + Points";
                 break;
 
-			default:
-				$for = "not found";
-				break;
-		}
+            default:
+                $for = "not found";
+                break;
+        }
 
-		return $for;
-	}
+        return $for;
+    }
 }
-?>
