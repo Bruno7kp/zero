e9135a71dbe4e7ce1c6f500bc51aaabaaeedc27d
bruno <bruno7kp@outlook.com>
2025-09-14 13:14:17 -0300

    newversion

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-define("MAIN_DIR", __DIR__."/");
-
-require_once("app/conf.php"); 
-
-B7KP\Library\Template::render();
--- src/B7KP/Interfaces/iPermission.php
+++
@@ -1,8 +0,0 @@
-<?php 
-namespace B7KP\Interfaces;
-
-interface iPermission
-{
-	public function permissionLevel($action, $field, $value);
-}
-?>
