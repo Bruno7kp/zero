cad8f24f24ab07bb269e08613ff8715216cb6edc
bruno <bruno7kp@outlook.com>
2025-09-26 16:34:52 -0300

    pwa

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'zero-icon.png', 'zero-bg.jpeg', 'zero-white.png', 'zero-black.png'],
+      // manifest será lido automaticamente de public/manifest.json
+    }),
+  ],
 })
